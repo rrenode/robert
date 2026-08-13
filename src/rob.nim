4b0450f4f82fc1f62024548653aa316afa60681a
rrenode <robert.renodej@gmail.com>
2026-08-13 15:34:42 -0400

    integrate new shell into frontend

--- src/rob.nim
+++ src/rob.nim
@@ -3,8 +3,13 @@ import karax/kdom except setInterval
 import karax/vstyles
 from sugar import `=>`
 
-import fs
+import std/parseopt
+import std/os
 
+
+import shell
+
+var rootShell: Shell = newShell("robert@renode")
 
 proc cmdWhoami(args: seq[string] = @[]): VNode =
   result = buildHtml(tdiv):
@@ -16,11 +21,6 @@ proc cmdWhoami(args: seq[string] = @[]): VNode =
       text "|-> Need something!"
     h2:
       text "|-> Math & Music Tutor"
-
-let root = FsDir(name: "/")
-root.addChild(
-  FsFile(name: "whoami", executable: true, execProc:cmdWhoami)
-)
 
 proc newCommandView(input: string, output: VNode): VNode =
   result = buildHtml(tdiv(class="command")):
@@ -48,15 +48,42 @@ proc createShellCursor: VNode =
         proc oninput(ev: Event, n: VNode) =
           curTyped = $InputElement(ev.target).value
         proc onkeyupenter(ev: Event, n: VNode) =
-          echo "PRESSED"
+          if curTyped.len != 0:
+            rootShell.dispatchCommandShell(curTyped)
+            curTyped = ""
     tdiv(class="command-output")
-    
+
+proc renderCommand(s: Shell, d: ShellOutput): VNode =
+  result = buildHtml(tdiv(class="command")):
+    p:
+      span(class="shell-prompt"):
+        text "robert@renode:~$ "
+      span:
+        text d.text
+
+proc renderInfo(s: Shell, d: ShellOutput): VNode =
+  result = buildHtml(tdiv(class="command-output")):
+    text d.text
+
+proc renderOutput(s: Shell): VNode =
+  result = buildHtml(tdiv):
+    for res in s.outputBuffer:
+      case res.kind
+      of sokCommand:
+        renderCommand(s, res)
+      of sokStdout:
+        discard
+      of sokStderr:
+        discard
+      of sokInfo:
+        renderInfo(s, res)
 
 proc createDom: VNode =
   result = buildHtml(tdiv):
     canvas(id="bg")
     main():
-      newCommandView("whoami", cmdWhoami())
+      #newCommandView("whoami", cmdWhoami())
+      renderOutput(rootShell)
       createShellCursor()
 
     
